890b2feebbc0165c0c1ab7a5834fa5f086e02f8c
Wei Lee <weilee.rx@gmail.com>
2021-06-07 10:40:36 +0800

    build(invoke): add doc_build task

--- tasks.py
+++ tasks.py
@@ -12,3 +12,13 @@ def init_dev(ctx):
 def format(ctx):
     ctx.run(f"{VENV_PREFIX} isort --atomic .")
     ctx.run(f"{VENV_PREFIX} black .")
+
+
+@task(optional=["clean"])
+def doc_build(ctx, clean=True):
+    """Build documentation locally"""
+    argument = ""
+    if clean:
+        argument += " --clean"
+
+    ctx.run(f"{VENV_PREFIX} mkdocs build {argument}")
