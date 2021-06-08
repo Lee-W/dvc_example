a1bcf5ede2d79204f5f648008278c1981ffcf3ac
Wei Lee <weilee.rx@gmail.com>
2021-06-08 17:32:47 +0800

    feat: split main into process-data, train, report commands

--- digit_recognizer/digit_recognizer.py
+++ digit_recognizer/digit_recognizer.py
@@ -1,5 +1,7 @@
+import argparse
 import csv
 import json
+import pickle
 
 from sklearn import metrics, svm
 from sklearn.model_selection import train_test_split
@@ -25,11 +27,32 @@ def process_data(X, y):
     return X_train, X_test, y_train, y_test
 
 
+def export_processed_data(data, output_path):
+    with open(output_path, "wb") as output_file:
+        pickle.dump(data, output_file)
+
+
+def load_processed_data(data_path):
+    with open(data_path, "rb") as data_file:
+        X, y = pickle.load(data_file)
+        return X, y
+
+
 def train_model(X_train, y_train):
     # Create a classifier: a support vector classifier
     clf = svm.SVC(gamma=0.001)
     # Learn the digits on the train subset
     return clf.fit(X_train, y_train)
+
+
+def export_model(model, output_path="output/model.pkl"):
+    with open(output_path, "wb") as model_output_file:
+        pickle.dump(model, model_output_file)
+
+
+def load_model(model, model_path="output/model.pkl"):
+    with open(model_path, "rb") as model_file:
+        return pickle.load(model_file)
 
 
 def output_metrics(y_test, predicted_y, output_filename: str = "output/metrics.json"):
@@ -63,12 +86,25 @@ def output_test_data_results(
 
 
 def main():
-    X, y = load_data("data/digit_data.csv", "data/digit_target.csv")
-    X_train, X_test, y_train, y_test = process_data(X, y)
-    model = train_model(X_train, y_train)
-    predicted_y = model.predict(X_test)
-    output_test_data_results(y_test, predicted_y)
-    output_metrics(y_test, predicted_y)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", help="Supported commands: process-data,train,report")
+    args = parser.parse_args()
+
+    if args.command == "process-data":
+        X, y = load_data("data/digit_data.csv", "data/digit_target.csv")
+        X_train, X_test, y_train, y_test = process_data(X, y)
+        export_processed_data((X_train, y_train), "output/training_data.pkl")
+        export_processed_data((X_test, y_test), "output/testing_data.pkl")
+    elif args.command == "train":
+        X_train, y_train = load_processed_data("output/training_data.pkl")
+        model = train_model(X_train, y_train)
+        export_model(model, "output/model.pkl")
+    elif args.command == "report":
+        X_test, y_test = load_processed_data("output/testing_data.pkl")
+        model = load_model("output/model.pkl")
+        predicted_y = model.predict(X_test)
+        output_test_data_results(y_test, predicted_y)
+        output_metrics(y_test, predicted_y)
 
 
 if __name__ == "__main__":
